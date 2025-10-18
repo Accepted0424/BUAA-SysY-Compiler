84ee3af87846fce3d473f45a557262313e985b41
lhy <23373112@buaa.edu.cn>
2025-10-18 15:37:05 +0800

    hw3: add hw3 release shell

--- myCompiler.sh
+++ myCompiler.sh
@@ -4,4 +4,8 @@ if [ "$1" == "release" ]; then
   if [ "$2" == "hw2" ]; then
     zip -r hw2.zip src CMakeLists.txt config.json
   fi
+
+  if [ "$2" == "hw3" ]; then
+    zip -r hw3.zip src CMakeLists.txt config.json
+  fi
 fi
